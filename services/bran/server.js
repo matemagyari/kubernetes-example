3851b43e5c28fec4016961b1ba053e0260f165fe
matemagyari <mate.magyari@gmail.com>
2018-06-12 11:58:08 +0200

    Game of Thrones

--- services/bran/server.js
+++ services/bran/server.js
@@ -4,38 +4,39 @@ var request = require('request-promise');
 
 function hodorServiceUrl() {
 
-    var environment = JSON.stringify(process.env, null, 4); // (Optional) beautiful indented output.
-    console.log('Environment', environment);
+//    var environment = JSON.stringify(process.env, null, 4); // (Optional) beautiful indented output.
+//    console.log('Environment', environment);
 
     var hodorHost = process.env.HODOR_SERVICE_HOST
-//    var hodorPort = process.env.HODOR_SERVICE_PORT
-    var hodorPort = 8080
+    var hodorPort = process.env.HODOR_SERVICE_PORT_HTTP
     return 'http://' + hodorHost + ':' + hodorPort
 }
 
 function askHodor() {
 
-    console.log('[' + new Date() + '] asking Hodor on [' + hodorServiceUrl() + ']')
+    var hodorService = hodorServiceUrl()
 
-//    var request = http.get(hodorServiceUrl(), (resp) => {
-//      let data = '';
-//
-//      // A chunk of data has been received.
-//      resp.on('data', (chunk) => {
-//        data += chunk;
-//      });
-//
-//      // The whole response has been received. Print out the result.
-//      resp.on('end', () => {
-//        console.log('Hodor said: ' + data);
-//      });
-//
-//    }).on("error", (err) => {
-//      console.log("Error: " + err.message);
-//    });
+    console.log('[' + new Date() + '] asking Hodor on [' + hodorService + ']')
 
-    var req = { "method":"GET", "uri": hodorServiceUrl() }
-    request(req).then(console.log, console.log);
+    var request = http.get(hodorService, (resp) => {
+      let data = '';
+
+      // A chunk of data has been received.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        console.log('Hodor said: [' + data + ']');
+      });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+
+//    var req = { "method":"GET", "uri": hodorService }
+//    request(req).then(console.log, console.log);
 }
 
 while (true) {
