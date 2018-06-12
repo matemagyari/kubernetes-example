61d49117a1c69d0de2a5853e83d8bd0f1bcac3d2
matemagyari <mate.magyari@gmail.com>
2018-06-12 12:48:53 +0200

    make it work

--- services/bran/server.js
+++ services/bran/server.js
@@ -1,6 +1,5 @@
-var http = require('http');
 var sleep = require('sleep');
-var request = require('request-promise');
+var request = require('sync-request');
 
 function hodorServiceUrl() {
 
@@ -12,31 +11,12 @@ function hodorServiceUrl() {
     return 'http://' + hodorHost + ':' + hodorPort
 }
 
+var hodorService = hodorServiceUrl()
+
 function askHodor() {
-
-    var hodorService = hodorServiceUrl()
-
     console.log('[' + new Date() + '] asking Hodor on [' + hodorService + ']')
-
-    var request = http.get(hodorService, (resp) => {
-      let data = '';
-
-      // A chunk of data has been received.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log('Hodor said: [' + data + ']');
-      });
-
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
-
-//    var req = { "method":"GET", "uri": hodorService }
-//    request(req).then(console.log, console.log);
+    var res = request('GET', hodorService);
+    console.log('Hodor said: [' + res.getBody() + ']');
 }
 
 while (true) {
